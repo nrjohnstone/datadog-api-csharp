00598f8f50bdeb7971f0ad2d91222f9f45ae3956
nrjohnstone <locotewz@gmail.com>
2016-07-29 08:33:08 +0200

    add description to assembly info

--- src/DataDog.Api/Properties/AssemblyInfo.cs
+++ src/DataDog.Api/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataDog.Api")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CSharp library for DataDog REST API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nathan Johnstone")]
 [assembly: AssemblyProduct("DataDog.Api")]
