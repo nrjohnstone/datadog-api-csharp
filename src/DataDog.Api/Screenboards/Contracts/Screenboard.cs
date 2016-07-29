792c199ff817158449c9dd1ec12adbcea3a6c538
nrjohnstone <locotewz@gmail.com>
2016-07-29 09:53:56 +0200

    implement end point for retrieving information about a specific screenboard

---
+++ src/DataDog.Api/Screenboards/Contracts/Screenboard.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DataDog.Api.Screenboards.Contracts
+{
+    public class Screenboard
+    {
+        public string BoardTitle { get; set; }
+        public string Width { get; set; }
+        public string Height { get; set; }
+        public bool ReadOnly { get; set; }
+        public string BoardBgtype { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string OriginalTitle { get; set; }
+        public DateTimeOffset Modified { get; set; }
+        public bool Templated { get; set; }
+        public bool Shared { get; set; }
+        public int Id { get; set; }
+        public bool TitleEdited { get; set; }
+    }
+}
